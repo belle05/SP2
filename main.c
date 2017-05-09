6c5ba3fb89cedc0dec4d385db820d3ce309c80d3
nufarlevy2 <levynufar@gmail.com>
2017-05-09 13:08:33 +0300

    Update main.c

--- main.c
+++ main.c
@@ -6,31 +6,31 @@
 #include <math.h>
 #include <string.h>
 #include <ctype.h>
+#include "sp_nim.h"
+#include "main_aux.h"
 #include "SPBufferset.h"
 
 int main()
 {
-	SP_BUFF_SET();
-	int *heapSizes;
-	int turn = 0;
-	int round = 0;
+        SP_BUFF_SET();
+        int turn = 0;
+        int round = 0;
 
-	int *heaps = start_game();
-	if (heaps==NULL){
-		return 0;
-	}
-	while(check_if_empty(heaps)){
-		print_status(heaps, round);
-		if (turn == 1) {
-			graph_status(heaps);
-			player_turn(heaps);
-		} else {
-			computer_turn(heaps);
-		}
-		if (turn == 0) turn = 1;
-		else (turn == 1) turn = 0;
-		round++;
-	}
-	end_game(turn);
+        int *heaps = start_game();
+        if (heaps==NULL){
+                return 0;
+        }
+        while(!(check_if_empty(heaps))){
+                print_status(heaps, round);
+                if (turn == 1) {
+                        graph_status(heaps);
+                        player_turn(heaps);
+                } else {
+                        computer_turn(heaps);
+                }
+                if (turn == 0) turn = 1;
+                else turn = 0;
+                round++;
+        }
+        end_game(turn);
 }
-
